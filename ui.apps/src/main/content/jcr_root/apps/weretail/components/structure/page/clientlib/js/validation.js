6a93797b73b73864b099bde66a0c230c3f34b414
fiste96 <62580970+fiste96@users.noreply.github.com>
2021-01-18 15:15:15 +0100

    Update validation.js

--- ui.apps/src/main/content/jcr_root/apps/weretail/components/structure/page/clientlib/js/validation.js
+++ ui.apps/src/main/content/jcr_root/apps/weretail/components/structure/page/clientlib/js/validation.js
@@ -13,5 +13,5 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  **************************************************************************/
-console.log('Ne znamDSADAS ni ja vise');
+console.log('Ne znam ni ja vise');
 
